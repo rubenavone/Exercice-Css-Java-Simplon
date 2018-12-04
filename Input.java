26558914c51ea14d3fd35afb9dde6a1d953f1d35
rubenavone <navone_ruben@msn.com>
2018-12-04 16:39:12 +0100

    first commit

---
+++ Input.java
@@ -0,0 +1,19 @@
+package inp;
+
+import java.util.Scanner; // importing the class scanner
+
+public class Input {
+	
+	 public static void main(String[] arg){
+	    	
+		    System.out.println("please insert your name !"); // ask user for is name 
+		    
+		    Scanner scan = new Scanner(System.in); // this call the class scan in your soft
+		    
+		    String inputName = scan.nextLine(); // create the inputName var = to the input of user
+		    
+		    System.out.println( "Hello" + " " + inputName); // printing the result 
+	
+	}
+
+}
